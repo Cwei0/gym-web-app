f8a5af4734c152ab7eb22eaaa93a0cd49c781466
Cwei0 <109186036+Cwei0@users.noreply.github.com>
2023-06-26 15:38:14 +0100

    feat: navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "./components";
-
+import { SelectedPage } from "./types";
 
 export const App = () => {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(isTopOfPage);
+        setSelectedPage(SelectedPage.Home);
+      }
+
+      if (window.scrollY !== 0) setIsTopOfPage(!isTopOfPage);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="app bg-gray-20">
-      <Navbar/>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
-  )
-}
+  );
+};
