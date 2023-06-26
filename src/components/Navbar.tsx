8929108398b1d853cc1afa3c329f5311fa5a89dd
Cwei0 <109186036+Cwei0@users.noreply.github.com>
2023-06-26 11:35:35 +0100

    Feat: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,11 @@ export const Navbar = (props: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}></div>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}></div>
+        </div>
+      </div>
     </nav>
   );
 };
